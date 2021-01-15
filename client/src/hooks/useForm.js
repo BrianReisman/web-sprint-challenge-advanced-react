0b425f608b86f4c7e8694cd307efc17f5f64dab7
BrianReisman <brianreisman@protonmail.com>
2021-01-15 11:43:32 -0500

    custom hook created

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,19 @@
 // write your custom hook here to control your checkout form
+import e from "cors";
+import { useState } from "react";
+
+export const useForm = (initialValue) => {
+  const [values, setFormValues] = useState(initialValue);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
+  const handleChanges = e => {
+    setFormValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setShowSuccessMessage(true);
+  }
+
+  return [values, handleChanges, handleSubmit, showSuccessMessage];
+};
