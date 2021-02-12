814b55edc7601e9b1d18ca9b5631ad50c6b7b2cd
BrianReisman <brianreisman@protonmail.com>
2021-02-12 12:32:28 -0500

    final commit MVP met

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -4,37 +4,29 @@ import CheckoutForm from "./CheckoutForm";
 import userEvent from '@testing-library/user-event';
 
 test("form header renders", () => {
-  //Arrange
   render(<CheckoutForm/>)
   const formHeader = screen.getByText(/Checkout foRm/i)
-  //Act
-  //n/a for this test
-  //Assert
   expect(formHeader).toBeInTheDocument();
 });
 
 test("form shows success message on submit with form details", async () => {
-  //Arrange
   render(<CheckoutForm/>)
-  const firstName = screen.getByLabelText(/first name/i);
-  const lastName = screen.getByLabelText(/last name/i);
-  const address = screen.getByLabelText(/address/i);
-  const city = screen.getByLabelText(/city/i);
-  const state = screen.getByLabelText(/STATE/i);
-  const zip = screen.getByLabelText(/zIp/i);
   const button = screen.getByRole('button');
+  const zip = screen.getByLabelText(/zIp/i);
+  const address = screen.getByLabelText(/address/i);
+  const state = screen.getByLabelText(/STATE/i);
+  const city = screen.getByLabelText(/city/i);
+  const lastName = screen.getByLabelText(/last name/i);
+  const firstName = screen.getByLabelText(/first name/i);
   
-  //Act
-  userEvent.type(firstName, 'test firstName');
-  userEvent.type(lastName, 'test lastName');
-  userEvent.type(address, '1234 test address');
-  userEvent.type(city, 'test cityName');
-  userEvent.type(state, 'test ST');
   userEvent.type(zip, '12345');
+  userEvent.type(state, 'test ST');
+  userEvent.type(city, 'test cityName');
   userEvent.click(button);
+  userEvent.type(address, '1234 test address');
+  userEvent.type(lastName, 'test lastName');
+  userEvent.type(firstName, 'test firstName');
   
-  //Assert
   const successMessage = await screen.findByTestId("successMessage");
-  expect(successMessage).toBeInTheDocument();
-  
-});
+  expect(successMessage).toBeInTheDocument();  
+})
