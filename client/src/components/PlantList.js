f99432802c19162917a66139d336046f2c8e53df
BrianReisman <brianreisman@protonmail.com>
2021-02-12 11:26:49 -0500

    custom hook

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -13,9 +13,6 @@ export default class PlantList extends Component {
         this.setState({plants: res.data.plantsData})
       })
   }
-  // when the component mounts:
-  //   - fetch data from the server endpoint - http://localhost:3333/plants
-  //   - set the returned plants array to this.state.plants
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
